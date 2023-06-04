6230a14b1dbbccdf789039f4193a1bdc148170b0
sagarsikha12 <68449419+sagarsikha12@users.noreply.github.com>
2023-06-04 21:51:29 +1000

    no errors

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -3,7 +3,7 @@ import "./CheckoutProduct.css";
 import { useStateValue } from "./StateProvider";
 
 function CheckoutProduct({ id, image, title, price, rating }) {
- const[{basket},dispatch]=useStateValue();
+ const[dispatch]=useStateValue();
 
 
  const removeFrombasket=()=>{
